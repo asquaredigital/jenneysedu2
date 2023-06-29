2d4aa78e7fb31ec46806800b7af7f288b945df60
asquaredigital <asquaredigitalsolutions@gmail.com>
2023-06-30 02:11:22 +0530

    commit application

--- apply.php
+++ apply.php
@@ -12,7 +12,7 @@ if ($_SERVER['REQUEST_METHOD'] !== 'POST') {
 $name = $_POST['name'];
 $dob = $_POST['dob'];
 $father_name = $_POST['father_name'];
-$Mother_name = $_POST['mother_name'];
+$mother_name = $_POST['mother_name'];
 $religion = $_POST['religion'];
 $community = $_POST['community'];
 $caste = $_POST['caste'];
@@ -25,11 +25,17 @@ $hsc_total = $_POST['hsc_total'];
 $hsc_passing = $_POST['hsc_passing'];
 $ug_subject = $_POST['ug_subject'];
 $ug_total = $_POST['ug_total'];
+$ug_obtained = $_POST['ug_obtained'];
 $ug_passing = $_POST['ug_passing'];
+$ug_percentage = $_POST['ug_percentage'];
 $pg_total = $_POST['pg_total'];
 $pg_subject = $_POST['pg_subject'];
+$pg_obtained = $_POST['pg_obtained'];
+$pg_percentage = $_POST['pg_percentage'];
 $pg_passing = $_POST['pg_passing'];
 $bed_subject = $_POST['bed_subject'];
+$bed_percentage = $_POST['bed_percentage'];
+$bed_obtained = $_POST['bed_obtained'];
 $bed_total = $_POST['bed_total'];
 $bed_passing = $_POST['bed_passing'];
 $mobile = $_POST['mobile'];
@@ -58,13 +64,19 @@ HSC Obtained Marks: $hsc_obtained\n
 HSC Total: $hsc_total\n
 HSC Year of Passing: $hsc_passing\n
 UG Subject: $ug_subject\n
+UG Obtained: $ug_obtained\n
 UG Total: $ug_total\n
+UG Percentage: $ug_percentage\n
 UG Passing Year: $ug_passing\n
 PG Subject: $pg_subject\n
+PG Obtained: $pg_obtained\n
 PG Total: $pg_total\n
+PG Percentage: $pg_percentage\n
 PG Passing Year: $pg_passing\n
 B.Ed Subject : $bed_subject\n
+B.Ed Obtained: $bed_obtained\n
 B.Ed Total: $bed_total\n
+B.Ed Percentage: $bed_percentage\n
 B.Ed Passing YEar: $bed_passing\n
 Mobile: $mobile\n
 Email: $email\n
